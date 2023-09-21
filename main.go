f96d3acf48cd42c9acc2359edae3d5814078f587
Dipjyoti Metia <dipjyotimetia@gmail.com>
2023-09-21 23:35:18 +1000

    update

--- main.go
+++ main.go
@@ -8,47 +8,8 @@ import (
 	"net/url"
 	"os"
 	"os/signal"
-	"sync"
 	"time"
 )
-
-const (
-	// The maximum number of requests allowed per second.
-	requestLimit = 100
-)
-
-type rateLimiter struct {
-	mu        sync.Mutex
-	limit     int
-	remaining int
-	lastReset time.Time
-}
-
-func newRateLimiter(limit int) *rateLimiter {
-	return &rateLimiter{
-		limit:     limit,
-		remaining: limit,
-		lastReset: time.Now(),
-	}
-}
-
-func (rl *rateLimiter) allow() bool {
-	rl.mu.Lock()
-	defer rl.mu.Unlock()
-
-	now := time.Now()
-	if now.Sub(rl.lastReset) > 1*time.Second {
-		rl.remaining = rl.limit
-		rl.lastReset = now
-	}
-
-	if rl.remaining == 0 {
-		return false
-	}
-
-	rl.remaining--
-	return true
-}
 
 var backendURLs = map[string]string{
 	"/api/v1/users":    "http://localhost:8081",
@@ -56,18 +17,11 @@ var backendURLs = map[string]string{
 }
 
 func main() {
-	// Create a rate limiter.
-	rateLimiter := newRateLimiter(requestLimit)
 
 	// Create a server to handle requests.
 	server := &http.Server{
 		Addr: ":8080",
 		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			// Check if the request is allowed.
-			if !rateLimiter.allow() {
-				w.WriteHeader(http.StatusTooManyRequests)
-				return
-			}
 			// Get the backend URL for the requested API path.
 			backendURL, ok := backendURLs[r.URL.Path]
 			if !ok {
